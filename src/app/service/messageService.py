5440ff0669e9706a0077d0ef3b416364116da82a
Pulla Maheswar Reddy <maheswar.reddy.pulla@sap.com>
2025-07-08 20:20:11 +0530

    feat: added functions to call llm and get response

---
+++ src/app/service/messageService.py
@@ -0,0 +1,15 @@
+from src.app.utils.messageUtil import MessageUtil
+
+from src.app.service.llmService import LLMService
+
+
+class MessageService:
+    def __init__(self):
+        self.messageUtil = MessageUtil()
+        self.llmService = LLMService()
+
+    def process_message(self, message):
+        if self.messageUtil.isBankSms(message):
+            return self.llmService.runLLM(message)
+        else:
+            return None
