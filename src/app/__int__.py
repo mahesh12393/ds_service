03080a31b412e847d8c27930accbb807780c0cda
Pulla Maheswar Reddy <maheswar.reddy.pulla@sap.com>
2025-07-10 18:57:46 +0530

    feat: exposed api to send expenses to kafka

--- src/app/__int__.py
+++ src/app/__int__.py
@@ -1,22 +1,33 @@
 from flask import Flask
-from flask import request,jsonify
+from flask import request, jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile("config.py")
 
 messageService = MessageService()
+producer = KafkaProducer(
+    bootstrap_servers=["localhost:9092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 
-@app.route('/v1/ds/message/', methods=['POST'])
+
+@app.route("/v1/ds/message/", methods=["POST"])
 def handle_message():
-    message = request.json.get('message')
+    message = request.json.get("message")
     result = messageService.process_message(message)
+    serialized_result = result.model_dump_json()
+
+    producer.send("expense_service", serialized_result)
     return jsonify(result)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def handle_get():
-    return 'Hello world'
+    return "Hello world"
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port= 8000 ,debug=True)
+    app.run(host="localhost", port=8000, debug=True)
